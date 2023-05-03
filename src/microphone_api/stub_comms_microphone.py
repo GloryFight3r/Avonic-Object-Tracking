55c200bbe181aa1d15187433d5a6317cbc71ff07
ismilenov <I.Y.Smilenov@student.tudelft.nl>
2023-05-03 16:11:17 +0200

    feat: add button to get microphone direction

--- src/microphone_api/stub_comms_microphone.py
+++ src/microphone_api/stub_comms_microphone.py
@@ -1,5 +1,18 @@
+'''
+TODO: when we have access to microphone it should be made functional
+Currently returns the two angles that are preset
+'''
+
 def get_azimuth(azimuth: int) -> int:
+
+
+    if not type(azimuth) is int:
+        raise Exception("Azimuth should be integer")
+
     return azimuth
 
 def get_elevation(elevation: int) -> int:
+
+    if not type(elevation) is int:
+        raise Exception("Elevation should be integer")
     return elevation
--- src/web-app/app.py
+++ src/web-app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from camera.camera_bp import camera_bp
 from home.home_bp import home_bp
+from microphone.microphone_bp import microphone_bp
 
 app = Flask(__name__)
 
@@ -8,3 +9,4 @@ app = Flask(__name__)
 # And then add them here, so could they work together
 app.register_blueprint(camera_bp, url_prefix='/camera')
 app.register_blueprint(home_bp, url_prefix='/')
+app.register_blueprint(microphone_bp, url_prefix = '/microphone')
