dbe4494d25616b4e5980f89293024012e270e4f2
ismilenov <I.Y.Smilenov@student.tudelft.nl>
2023-05-04 17:07:04 +0200

    fix:remove unused tests

--- tests/test_microphone_bp.py
+++
@@ -1,9 +0,0 @@
-from web_app.microphone.microphone_bp import * 
-from microphone_api.stub_comms_microphone import *
-
-def test_get_direction():
-    
-    req = get_direction()
-    data = req.response
-    assert req.status_code == 200 
-
