07e5ebd41dad3aed56adb06f942cbd37066cfc2f
Petr Khartskhaev <p.khartskhaev@student.tudelft.nl>
2023-05-05 16:55:26 +0200

    test: changed microphone api tests for the real thing

--- tests/test_microphone_api.py
+++ tests/test_microphone_api.py
@@ -1,20 +1,59 @@
-from microphone_api.stub_comms_microphone import *
+from microphone_api.microphone_control_api import *
 import pytest
+from unittest import mock
+
 
 def test_azimuth():
-    api = MicrophoneAPI()
-    assert api.get_azimuth(46) == 46 
+    sock = mock.Mock()
+    sock.sendto.return_value = 48
+    sock.recvfrom.return_value = (bytes('{"m":{"beam":{"azimuth":46}}}\r\n', "ascii"), None)
+    api = MicrophoneAPI(UDPSocket(None, sock))
+    assert api.get_azimuth() == 46
 
-def test_azimuth_invalid():
-    api = MicrophoneAPI()
+def test_azimuth_recv_error():
+    """ Invalid command
+    """
+    sock = mock.Mock()
+    sock.sendto.return_value = 48
+    sock.recvfrom.return_value = (bytes('{"osc":{"error":[400,{"desc":"message not understood"}]}}\r\n', "ascii"), None)
+    api = MicrophoneAPI(UDPSocket(None, sock))
     with pytest.raises(Exception):
-        api.get_azimuth(56.7)
+        api.get_azimuth()
 
-def test_elevation(): 
-    api = MicrophoneAPI()
-    assert api.get_elevation(90) == 90
 
-def test_elevation_invalid():
-    api = MicrophoneAPI()
+def test_azimuth_sendto_error():
+    """ Invalid address given
+    """
+    api = MicrophoneAPI(UDPSocket(None))
     with pytest.raises(Exception):
-        api.get_elevation(56.7)
+        api.get_azimuth()
+
+
+def test_elevation():
+    sock = mock.Mock()
+    sock.sendto.return_value = 48
+    sock.recvfrom.return_value = (bytes('{"m":{"beam":{"elevation":90}}}\r\n', "ascii"), None)
+    api = MicrophoneAPI(UDPSocket(None, sock))
+    assert api.get_elevation() == 90
+
+def test_direction():
+    sock = mock.Mock()
+    sock.sendto.return_value = 48
+    sock.recvfrom.return_value = (bytes('{"m":{"beam":{"azimuth":0,"elevation":0}}}\r\n', "ascii"), None)
+    api = MicrophoneAPI(UDPSocket(None, sock))
+    assert api.get_direction() == [0.0, 0.0, 1.0]
+
+def test_speaking():
+    sock = mock.Mock()
+    sock.sendto.return_value = 48
+    sock.recvfrom.return_value = (bytes('{"m":{"in1":{"peak":-5}}}\r\n', "ascii"), None)
+    api = MicrophoneAPI(UDPSocket(None, sock))
+    assert api.is_speaking() == True
+
+
+def test_not_speaking():
+    sock = mock.Mock()
+    sock.sendto.return_value = 48
+    sock.recvfrom.return_value = (bytes('{"m":{"in1":{"peak":-70}}}\r\n', "ascii"), None)
+    api = MicrophoneAPI(UDPSocket(None, sock))
+    assert api.is_speaking() == False
