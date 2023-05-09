952edd71d9706f8567de8d2e521bbd8c49d9685d
Izzy van der Giessen <i.vandergiessen@student.tudelft.nl>
2023-05-09 15:49:17 +0200

    fix: move microphone test to separate file

--- tests/microphone_test/test_microphone_api.py
+++ tests/microphone_test/test_microphone_api.py
@@ -3,12 +3,6 @@ import pytest
 from microphone_api.microphone_adapter import Microphone
 from microphone_api.microphone_control_api import MicrophoneAPI
 
-def test_set_height_adapter():
-    mic = Microphone()
-    assert mic.height == 0
-    mic.set_height(10)
-    assert mic.height == 10
-
 def test_set_height_api():
     mic = Microphone()
     api = MicrophoneAPI(mic)
