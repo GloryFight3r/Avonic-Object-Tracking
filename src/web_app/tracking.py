9a48ed0268e5169dd79d5659f03bd628b1e42c90
Yehor Kozyr <y.kozyr@student.tudelft.nl>
2023-05-10 10:35:21 +0200

    Resolve "Refactor: structure of the Flask app"

---
+++ src/web_app/tracking.py
@@ -0,0 +1,14 @@
+from threading import Event
+from flask import make_response, jsonify, request
+
+def start_thread_endpoint(event: Event):
+    # start (unpause) the thread
+    print("Started thread")
+    event.set()
+    return make_response(jsonify({}), 200)
+
+def stop_thread_endpoint(event: Event):
+    # start (unpause) the thread
+    print("Stopping thread")
+    event.clear()
+    return make_response(jsonify({}), 200)
