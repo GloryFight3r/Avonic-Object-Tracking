b86c15b60d1267151dd4e3cf8c5fb54ee1106740
Yehor Kozyr <y.kozyr@student.tudelft.nl>
2023-05-10 12:04:29 +0200

    test: added test and changed the Flask app structure slightly

--- src/web_app/tracking.py
+++ src/web_app/tracking.py
@@ -1,16 +1,28 @@
 from threading import Event
 from flask import make_response, jsonify
+from avonic_speaker_tracker.custom_thread import CustomThread
 
 
-def start_thread_endpoint(event: Event):
+def start_thread_endpoint(event: Event, thread):
     # start (unpause) the thread
     print("Started thread")
-    event.set()
+    if thread[0] is None:
+        thread[0] = CustomThread(event)
+        thread[0].set_calibration(2)
+        event.clear()
+        thread[0].start()
+    else:
+        old_calibration = thread[0].value
+        thread[0] = CustomThread(event)
+        thread[0].set_calibration(old_calibration)
+        event.clear()
+        thread[0].start()
     return make_response(jsonify({}), 200)
 
 
-def stop_thread_endpoint(event: Event):
+def stop_thread_endpoint(event: Event, thread):
     # start (unpause) the thread
     print("Stopping thread")
-    event.clear()
+    event.set()
+    thread[0].join()
     return make_response(jsonify({}), 200)
---
+++ tests/web_app_test/test_flask.py
@@ -0,0 +1,15 @@
+import pytest
+import web_app
+
+@pytest.fixture
+def client():
+    """A test client for the app."""
+    app = web_app.create_app()
+    return app.test_client()
+
+
+def test_fail(client):
+    """Start with a blank database."""
+
+    rv = client.get('/fail-me')
+    assert rv.status_code == 418
