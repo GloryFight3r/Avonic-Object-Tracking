826e649fd904502ff133fdd9f99bd1c062d47ec0
Yehor Kozyr <y.kozyr@student.tudelft.nl>
2023-05-16 15:12:24 +0200

    fix: fixed names, long lines and documentation

--- src/avonic_speaker_tracker/coordinate_translation.py
+++ src/avonic_speaker_tracker/coordinate_translation.py
@@ -1,12 +1,20 @@
 import numpy as np
 
-def translate_microphone_to_camera_vector(camera_to_microphone: np.array, from_microphone_to_speaker: np.array, from_speaker_ceiling_distance: float):
-    if len(camera_to_microphone.shape) > 2 or (camera_to_microphone.shape[0] != 3) or (len(camera_to_microphone.shape) == 2 and camera_to_microphone.shape[1] != 1):
+def translate_microphone_to_camera_vector(cam_to_mic: np.array, mic_to_speaker: np.array, mic_height: float):
+    """ This method calculates the vector of the real distance from camera to the speaker.
+
+    Args:
+        cam_to_mic - calibration vector of the real distance from camera to microphone
+        mic_to_speaker - vector determined by the microphone
+        mic_height - distance between the plane of microphone to the plane of a speaker
+    """
+    if len(cam_to_mic.shape) > 2 or (cam_to_mic.shape[0] != 3) \
+        or (len(cam_to_mic.shape) == 2 and cam_to_mic.shape[1] != 1):
         raise TypeError("Not a 3D vector")
-    if len(from_microphone_to_speaker.shape) > 2 or (from_microphone_to_speaker.shape[0] != 3) or (len(from_microphone_to_speaker.shape) == 2 and from_microphone_to_speaker.shape[1] != 1):
+    if len(mic_to_speaker.shape) > 2 or (mic_to_speaker.shape[0] != 3) \
+        or (len(mic_to_speaker.shape) == 2 and mic_to_speaker.shape[1] != 1):
         raise TypeError("Not a 3D vector")
     
-    microphone_with_speaker_plane_intersection_point = from_microphone_to_speaker * from_speaker_ceiling_distance / abs(from_microphone_to_speaker[1])
-    from_camera_to_speaker = microphone_with_speaker_plane_intersection_point + camera_to_microphone
+    intersection_point = mic_to_speaker * mic_height / abs(mic_to_speaker[1])
 
-    return from_camera_to_speaker
+    return intersection_point + cam_to_mic
