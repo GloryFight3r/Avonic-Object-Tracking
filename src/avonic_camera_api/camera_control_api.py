fix: return vector instead of radians in get_direction

src/avonic_camera_api/camera_control_api.py
@@ -182,7 +182,7 @@ class CameraAPI:
             tilt_adjusted = -((tilt ^ ((1 << 16) - 1)) + 1)
         pan_rad = pan_adjusted * 0.0625 / 180 * math.pi
         tilt_rad = tilt_adjusted * 0.0625 / 180 * math.pi
-        return np.array([pan_rad, tilt_rad])
+        return converter.angle_vector(pan_rad, tilt_rad)
 
 
 def insert_zoom_in_hex(msg: str, zoom: int) -> str:

src/avonic_speaker_tracker/calibration.py
@@ -56,7 +56,7 @@ class Calibration:
             returns:
                 the 3D vector from the microphone to the camera
         """
-        cam_vecw = angle_vector(self.speaker_point[0][0], self.speaker_point[0][1])
+        cam_vecw = self.speaker_point[0]
         mic_vecw = self.speaker_point[1]
         assert mic_vecw[1] != 0.0 and self.mic_height != 0.0
 
@@ -65,10 +65,9 @@ class Calibration:
 
         # calculate the two angles needed
         alpha = angle_between_vectors(cam_vecw, mic_vecw)
-        beta = angle_between_vectors(cam_vecw, angle_vector(self.to_mic_direction[0], self.to_mic_direction[1]))
+        beta = angle_between_vectors(cam_vecw, self.to_mic_direction)
         assert beta != 0.0
 
         mic_to_cam_dist = np.linalg.norm(mic_vec) / np.sin(beta) * np.sin(alpha)
-        to_mic = angle_vector(self.to_mic_direction[0], self.to_mic_direction[1])
-        self.mic_to_cam = to_mic / np.linalg.norm(to_mic) * -mic_to_cam_dist
+        self.mic_to_cam = self.to_mic_direction / np.linalg.norm(self.to_mic_direction) * -mic_to_cam_dist
         return self.mic_to_cam

tests/camera_test/test_camera_control.py
@@ -1,6 +1,7 @@
 import pytest
 from avonic_camera_api.camera_control_api import CameraAPI
 from avonic_camera_api.camera_adapter import Camera
+from avonic_camera_api import converter
 import socket
 import math
 import numpy as np
@@ -224,4 +225,5 @@ def test_get_direction(monkeypatch, camera, direction, ret_msg):
     monkeypatch.setattr(camera.camera.sock, "sendall", mocked_send)
     monkeypatch.setattr(camera.camera.sock, "recv", mocked_return)
     monkeypatch.setattr(camera.camera.sock, "settimeout", mocked_timeout)
-    assert (camera.get_direction() == np.array(direction) * 0.0625 / 180 * math.pi).all()
+    direction =  np.array(direction) * 0.0625 / 180 * math.pi
+    assert (camera.get_direction() == converter.angle_vector(direction[0], direction[1])).all()

tests/tracker_test/test_calibration.py
@@ -61,14 +61,35 @@ def test_is_calibrated():
 
 def generate_calibration_setup():
     return [
-            (1, (np.array([0, 0]), np.array([0, -1, 0])), np.array([0, math.pi/4]), np.array([0, -1/2**0.5, -1/2**0.5]), np.array([0, -1, -1])),
-            (1, (np.array([0, 0]), np.array([0, -1, 0])), np.array([math.pi/4, 0]), np.array([-1/2**0.5, -1/2**0.5, 0]), np.array([-1, 0, -1])),
-            (2, (np.array([0, -math.pi/4]), np.array([0, -1, 0])), np.array([0, math.pi/4]), np.array([0, -1/2**0.5, -1/2**0.5]), np.array([0, -1, -1])),
-            (4, (np.array([0, -0.245]), np.array([0, -4, 0])), np.array([0, 0.6435]), np.array([0, -3, -4]), np.array([0, -3, -4]))
+            (1,
+                np.array([0.0, 2**0.5/2, 2**0.5/2]),
+                (np.array([0., 0., 1.]), np.array([0, -1, 0])),
+                #np.array([0, -1/2**0.5, -1/2**0.5]),
+                np.array([0, -1, -1])
+             ),
+            (1,
+                np.array([2**0.5/2, 0, 2**0.5/2]),
+                (np.array([0., 0., 1.]), np.array([0, -1, 0])),
+                #np.array([-1/2**0.5, -1/2**0.5, 0]),
+                np.array([-1, 0, -1])
+             ),
+            (2,
+                np.array([0, 2**0.5/2, 2**0.5/2]),
+                (np.array([0, -2**0.5/2, 2**0.5/2]), np.array([0, -1, 0])),
+                #np.array([0, -1/2**0.5, -1/2**0.5]),
+                np.array([0, -1, -1])
+             ),
+            #(4, (np.array([0, -0.245]), np.array([0, -4, 0])), np.array([0, 0.6435]), np.array([0, -3, -4]), np.array([0, -3, -4]))
+            (4,
+                np.array([0.0, 0.6, 0.8]),
+                (np.array([0, -0.24255632, 0.97013732]), np.array([0, -4, 0])),
+                #np.array([0, -3, -4]),
+                np.array([0, -3, -4])
+             )
     ]
 
-@pytest.mark.parametrize("height, speaker, to_mic, to_cam, mic_to_cam", generate_calibration_setup())
-def test_calculate_distance(height, speaker, to_mic, to_cam, mic_to_cam):
+@pytest.mark.parametrize("height, to_mic, speaker, mic_to_cam", generate_calibration_setup())
+def test_calculate_distance(height, to_mic, speaker, mic_to_cam):
     cal = Calibration()
     cal.set_height(height)
     cal.add_speaker_point(speaker)
@@ -82,8 +103,8 @@ def test_calculate_distance(height, speaker, to_mic, to_cam, mic_to_cam):
 def test_calculate_distance_one_line():
     cal = Calibration()
     cal.set_height(1)
-    cal.add_speaker_point((np.array([math.pi/2, 0]), np.array([0, -1, 0])))
-    cal.add_direction_to_mic(np.array([math.pi/2, 0]))
+    cal.add_speaker_point((np.array([1, 1, 0]), np.array([0, -1, 0])))
+    cal.add_direction_to_mic(np.array([1, 1, 0]))
 
     with pytest.raises(AssertionError):
         cal.calculate_distance()
@@ -91,13 +112,13 @@ def test_calculate_distance_one_line():
 def test_zero_height():
     cal = Calibration()
     cal.set_height(0)
-    cal.add_speaker_point((np.array([0, -0.245]), np.array([0, -4, 0])))
-    cal.add_direction_to_mic(np.array([0, 0.6435]))
+    cal.add_speaker_point((np.array([0, -0.24255632, 0.97013732]), np.array([0, -4, 0])))
+    cal.add_direction_to_mic(np.array([0, 0.6, 0.8]))
 
     with pytest.raises(AssertionError):
         cal.calculate_distance()
 
-    cal.add_speaker_point((np.array([0, -0.245]), np.array([1, 0, 0])))
+    cal.add_speaker_point((np.array([0, -0.24255632, 0.97013732]), np.array([1, 0, 0])))
 
     with pytest.raises(AssertionError):
         cal.calculate_distance()