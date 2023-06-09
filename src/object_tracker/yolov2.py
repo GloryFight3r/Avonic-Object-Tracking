a02c680b8436c538241d7fc8ce7e7e8a73659f4d
Izzy van der Giessen <i.vandergiessen@student.tudelft.nl>
2023-06-09 15:23:02 +0200

    test: add yolo tests

--- src/object_tracker/yolov2.py
+++ src/object_tracker/yolov2.py
@@ -4,25 +4,14 @@ from ultralytics import YOLO
 import torch
 
 class YOLOPredict:
-    model = None
-
     def __init__(self):
         self.model = YOLO("yolov8m.pt")
-
-    def get_bounding_boxes_image(self, frame):
-        results = self.model(frame, device="mps", classes=0)
-        result = results[0]
-
-        bboxes = np.array(result.boxes.xyxy.cpu(), dtype='int')
-        for x in bboxes:
-            (x, y, x2, y2) = x
-            self.draw_prediction(frame, "Person", x, y, x2, y2)
-
-        return frame
 
     def get_bounding_boxes(self, frame):
         results = self.model.predict(frame, classes=0, device="cpu")
         result = results[0]
+
+        # change 0.0 to a higher number to filter low confidence boxes
         conf_indices = set(torch.nonzero(result.boxes.conf.cpu() > 0.0))
         person_indices = set(torch.nonzero(result.boxes.cls.cpu() == 0))
 
