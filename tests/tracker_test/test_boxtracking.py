1a370cc90315c64ad6502d4f00894c1d75521d55
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2023-06-12 16:55:30 +0200

    fix: fixed failing tests

--- tests/tracker_test/test_boxtracking.py
+++ tests/tracker_test/test_boxtracking.py
@@ -1,8 +1,9 @@
 import pytest
 import numpy as np
 
-from avonic_speaker_tracker.boxtracking import BoxTracker
+from avonic_camera_api.footage import FootageThread
 from avonic_camera_api.camera_control_api import CameraAPI
+from avonic_speaker_tracker.utils.camera_navigation_utils import get_movement_to_box
 
 def generate_tests():
     return [
@@ -12,22 +13,26 @@ def generate_tests():
     ]
 
 @pytest.fixture
-def box_tracker():
+def cam_api():
     cam_api = CameraAPI(None)
-    resolution = np.array([1920.0, 1080.0])
-    return BoxTracker(cam_api, resolution)
+    return cam_api
+
+@pytest.fixture
+def cam_footage():
+    cam_footage = FootageThread(None, None)
+    return cam_footage
 
 @pytest.mark.parametrize("fov, bbox, expected", generate_tests())
-def test_camera_track(box_tracker, fov, bbox, expected, monkeypatch):
+def test_camera_track(cam_api, cam_footage, fov, bbox, expected, monkeypatch):
     def mocked_move(speed_one, speed_two, alpha, beta):
         assert np.array_equal(np.array([speed_one, speed_two, alpha, beta]), expected)
     def mocked_fov():
         return fov
 
-    monkeypatch.setattr(box_tracker.cam, "move_relative", mocked_move)
-    monkeypatch.setattr(box_tracker.cam, "calculate_fov", mocked_fov)
+    monkeypatch.setattr(cam_api, "move_relative", mocked_move)
+    monkeypatch.setattr(cam_api, "calculate_fov", mocked_fov)
 
-    box_tracker.camera_track(bbox)
+    get_movement_to_box(bbox, cam_api, cam_footage)
 
 def generate_errors():
     return [
@@ -40,14 +45,14 @@ def generate_errors():
 
 
 @pytest.mark.parametrize("fov, bbox", generate_errors())
-def test_camera_assertions(box_tracker, fov, bbox, monkeypatch):
+def test_camera_assertions(cam_api, cam_footage, fov, bbox, monkeypatch):
     def mocked_move(speed_one, speed_two, alpha, beta):
         pass
     def mocked_fov():
         return fov
 
-    monkeypatch.setattr(box_tracker.cam, "move_relative", mocked_move)
-    monkeypatch.setattr(box_tracker.cam, "calculate_fov", mocked_fov)
+    monkeypatch.setattr(cam_api, "move_relative", mocked_move)
+    monkeypatch.setattr(cam_api, "calculate_fov", mocked_fov)
 
     with pytest.raises(AssertionError):
-        box_tracker.camera_track(bbox)
+        get_movement_to_box(bbox, cam_api, cam_footage)
