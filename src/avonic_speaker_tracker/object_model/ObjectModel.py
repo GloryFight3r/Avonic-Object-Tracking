9f3b5e1db80f8222d1de70812ef438fd48c653a5
Izzy van der Giessen <i.vandergiessen@student.tudelft.nl>
2023-06-23 11:31:19 +0200

    fix: fix type errors that broke the thread

--- src/avonic_speaker_tracker/object_model/ObjectModel.py
+++ src/avonic_speaker_tracker/object_model/ObjectModel.py
@@ -126,7 +126,7 @@ class ObjectModel():
 
         speedAlpha = math.fabs(rotate_angle[0])/340.0*11.0+13.0
         speedBeta = math.fabs(rotate_angle[1])/120.0*9.0+11.0
-        return np.array([int(speedAlpha), int(speedBeta)])
+        return [int(speedAlpha), int(speedBeta)]
 
     def track_object(self):
         pass
